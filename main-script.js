8d509f2cb4e3a7197a5596fde018c59bc820c027
aisalkyn4160 <aisalkyn.4160@gmail.com>
2024-12-26 02:23:32 +0700

    adding projects slider

--- main-script.js
+++ main-script.js
@@ -12,6 +12,31 @@ const swiper = new Swiper('.banner-swiper.swiper', {
 
 });
 
+const projectsSwiper = new Swiper('.projects-swiper.swiper', {
+    loop: true,
+
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1, 
+        },
+        500: {
+            slidesPerView: 2,
+        },
+        800: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        }
+    },
+    spaceBetween: 24,
+});
+
 const swiper2 = new Swiper('.sertificates-swiper.swiper', {
     // loop: true,
   
@@ -25,8 +50,9 @@ const swiper2 = new Swiper('.sertificates-swiper.swiper', {
         320: {
             slidesPerView: 1,
         },
-        768: {
+        500: {
             slidesPerView: 2,
+            spaceBetween: 16,
         },
         1024: {
             slidesPerView: 3,
@@ -47,6 +73,9 @@ const swiper3 = new Swiper('.partners-swiper.swiper', {
     breakpoints: {
         320: {
             slidesPerView: 1, 
+        },
+        500: {
+            slidesPerView: 2,
         },
         768: {
             slidesPerView: 3,
@@ -69,7 +98,7 @@ const reviewsSwiper = new Swiper('.reviews-swiper.swiper', {
       320: {
           slidesPerView: 1,
       },
-      768: {
+      765: {
           slidesPerView: 2,
       },
       1024: {
